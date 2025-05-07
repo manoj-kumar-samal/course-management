54765bcd9892fa77215437b3582472a14a44982a
unknown <manojkumarsamal2002@gmail.com>
2025-05-07 11:16:23 +0530

    Re-commit without secret

--- Backend/config.js
+++ Backend/config.js
@@ -2,7 +2,8 @@ import dotenv from "dotenv";
 dotenv.config()
 const JWT_USER_PASSWORD= process.env.JWT_USER_PASSWORD;
 const JWT_ADMIN_PASSWORD=process.env.JWT_ADMIN_PASSWORD;
-const STRIPE_SCRET_KEY="sk_test_51RJDRSQ7CkXEwtPDUTiDZRT85aJc14yR76bNMsP2Uec1fpieGvKX9BI8iei7HT4UFa88NRohoFXk5wu4KIMmgtnM00jVg2H9pd"
+
+const STRIPE_SCRET_KEY = process.env.STRIPE_SCRET_KEY;
 
 export default{
     JWT_USER_PASSWORD,
